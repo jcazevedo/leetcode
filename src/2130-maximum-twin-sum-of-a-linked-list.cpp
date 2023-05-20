43ff84526a41bfd9d86d169a49a56b109406ebed
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-05-20 02:05:25 +0100

    Add solution to "Maximum Twin Sum of a Linked List"

---
+++ src/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -0,0 +1,41 @@
+// 2130. Maximum Twin Sum of a Linked List
+// https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+
+#include <algorithm>
+
+using namespace std;
+
+#define MAXN 100000
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  int pairSum(ListNode* head) {
+    int sums[MAXN];
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    int n = 0;
+    while (fast != nullptr) {
+      sums[n++] = slow->val;
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    int ans = 0;
+    while (n > 0) {
+      sums[--n] += slow->val;
+      ans = max(ans, sums[n]);
+      slow = slow->next;
+    }
+
+    return ans;
+  }
+};

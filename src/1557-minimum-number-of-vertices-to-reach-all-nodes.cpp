4982c1009f2cafd3d41bfd34988a670c438f6d3e
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-05-20 01:49:29 +0100

    Add solution to "Minimum Number of Vertices to Reach All Nodes"

---
+++ src/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,43 @@
+// 1557. Minimum Number of Vertices to Reach All Nodes
+// https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+  void dfs(int curr, int start, const vector<vector<int>>& graph, vector<bool>& visited, set<int>& minSet) {
+    if (visited[curr]) {
+      minSet.erase(curr);
+      minSet.insert(start);
+      return;
+    }
+
+    visited[curr] = true;
+    for (int next : graph[curr])
+      dfs(next, start, graph, visited, minSet);
+  }
+
+public:
+  vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+    vector<bool> visited = vector<bool>(n, false);
+    vector<vector<int>> graph = vector<vector<int>>(n, vector<int>());
+    for (const vector<int>& edge : edges)
+      graph[edge[0]].push_back(edge[1]);
+
+    set<int> minSet;
+    for (int i = 0; i < n; ++i)
+      if (!visited[i]) {
+        minSet.insert(i);
+        dfs(i, i, graph, visited, minSet);
+      }
+
+    vector<int> ans;
+    for (int i : minSet)
+      ans.push_back(i);
+
+    return ans;
+  }
+};

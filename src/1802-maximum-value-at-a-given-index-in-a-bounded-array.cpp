3724972841f357ebedf3f4fa3238c671d29637fa
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-06-11 18:36:29 +0100

    Remove unused import

--- src/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ src/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,10 +1,6 @@
 // 1802. Maximum Value at a Given Index in a Bounded Array
 // https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/
 
-#include <iostream>
-
-using namespace std;
-
 class Solution {
 private:
   long long sumTo(long long n) { return n * (n + 1) / 2; }

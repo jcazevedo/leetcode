8e3e5a0e724df57d0d6a6aa47fc8c076b285852d
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-06-14 09:30:45 +0100

    Add solution to "Minimum Distance Between BST Nodes"

---
+++ src/0783-minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,36 @@
+// 783. Minimum Distance Between BST Nodes
+// https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+#include <algorithm>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+  long long getMinimumDifferenceAux(TreeNode* root, long long left, long long right) {
+    long long diffLeft = abs(root->val - left);
+    long long diffRight = abs(root->val - right);
+    long long ans = min(diffLeft, diffRight);
+    if (root->left != nullptr)
+      ans = min(ans, getMinimumDifferenceAux(root->left, left, root->val));
+    if (root->right != nullptr)
+      ans = min(ans, getMinimumDifferenceAux(root->right, root->val, right));
+    return ans;
+  }
+
+public:
+  int minDiffInBST(TreeNode* root) {
+    return getMinimumDifferenceAux(root, numeric_limits<int>::min(), numeric_limits<int>::max());
+  }
+};

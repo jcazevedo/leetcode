98d375cd59df8c0d7c8773ccd4faede94c7e0d9e
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-06-15 01:17:34 +0100

    Add solution to "Maximum Level Sum of a Binary Tree"

---
+++ src/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -0,0 +1,44 @@
+// 1161. Maximum Level Sum of a Binary Tree
+// https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+
+#include <algorithm>
+#include <limits>
+#include <unordered_map>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+  int levels = 0;
+  unordered_map<int, int> levelSum;
+
+  void go(TreeNode* curr, int level) {
+    levels = max(levels, level);
+    levelSum[level] += curr->val;
+    if (curr->left != nullptr)
+      go(curr->left, level + 1);
+    if (curr->right != nullptr)
+      go(curr->right, level + 1);
+  }
+
+public:
+  int maxLevelSum(TreeNode* root) {
+    go(root, 1);
+    int ans = 0, best = numeric_limits<int>::min();
+    for (int level = 1; level <= levels; ++level)
+      if (levelSum[level] > best) {
+        best = levelSum[level];
+        ans = level;
+      }
+    return ans;
+  }
+};

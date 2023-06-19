f726f188e68e18dc076b47a018130c36cdd5540c
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-06-19 07:53:46 +0100

    Add solution to "Find the Highest Altitude"

---
+++ src/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,19 @@
+// 1732. Find the Highest Altitude
+// https://leetcode.com/problems/find-the-highest-altitude/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int largestAltitude(vector<int>& gain) {
+    int ans = 0, curr = 0;
+    for (int v : gain) {
+      curr += v;
+      ans = max(ans, curr);
+    }
+    return ans;
+  }
+};

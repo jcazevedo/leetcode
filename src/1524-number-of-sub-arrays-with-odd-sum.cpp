9faea10c19addef895a0627cb187c3c9eab1f3e4
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-06-25 17:12:04 +0100

    Add solution to "Number of Sub-arrays With Odd Sum"

---
+++ src/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,29 @@
+// 1524. Number of Sub-arrays With Odd Sum
+// https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/
+
+#include <vector>
+
+using namespace std;
+
+#define MOD 1000000007
+
+class Solution {
+public:
+  int numOfSubarrays(vector<int>& arr) {
+    int N = arr.size(), currOdd = 0, currEven = 0, ans = 0;
+    for (int i = 0; i < N; ++i) {
+      int nextOdd, nextEven;
+      if (arr[i] % 2 == 0) {
+        nextEven = (1 + currEven) % MOD;
+        nextOdd = currOdd;
+      } else {
+        nextOdd = (1 + currEven) % MOD;
+        nextEven = currOdd;
+      }
+      currOdd = nextOdd;
+      currEven = nextEven;
+      ans = (ans + currOdd) % MOD;
+    }
+    return ans;
+  }
+};

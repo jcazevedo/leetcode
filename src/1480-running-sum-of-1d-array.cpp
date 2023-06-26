cacb40ef6578c463d995888bf30c465e98a2519a
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-06-27 00:02:14 +0100

    Add solution to "Running Sum of 1d Array"

---
+++ src/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,17 @@
+// 1480. Running Sum of 1d Array
+// https://leetcode.com/problems/running-sum-of-1d-array/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> runningSum(vector<int>& nums) {
+    vector<int> ans = vector<int>(nums.size(), 0);
+    ans[0] = nums[0];
+    for (int i = 1; i < (int)nums.size(); ++i)
+      ans[i] = ans[i - 1] + nums[i];
+    return ans;
+  }
+};

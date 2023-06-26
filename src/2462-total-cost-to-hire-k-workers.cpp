a2495b9422b41469fe09494fce52b345b8d05cc3
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-06-26 09:40:52 +0100

    Add solution to "Total Cost to Hire K Workers"

---
+++ src/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,36 @@
+// 2462. Total Cost to Hire K Workers
+// https://leetcode.com/problems/total-cost-to-hire-k-workers/
+
+#include <set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  long long totalCost(vector<int>& costs, int k, int candidates) {
+    set<pair<int, int>> totCandidates;
+    for (int i = 0; i < candidates; ++i)
+      totCandidates.insert(make_pair(costs[i], i));
+    for (int i = costs.size() - 1; i >= (int)costs.size() - candidates; --i)
+      totCandidates.insert(make_pair(costs[i], i));
+    int il = candidates, ir = costs.size() - candidates - 1;
+    long long ans = 0;
+    while (k-- > 0) {
+      pair<int, int> choice = *totCandidates.begin();
+      ans += costs[choice.second];
+      totCandidates.erase(make_pair(choice.first, choice.second));
+      if (il <= ir) {
+        if (choice.second < il) {
+          totCandidates.insert(make_pair(costs[il], il));
+          il++;
+        } else {
+          totCandidates.insert(make_pair(costs[ir], ir));
+          ir--;
+        }
+      }
+    }
+    return ans;
+  }
+};

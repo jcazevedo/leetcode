c2b26bbe751a0ea3b22148b3150fe1a350f8aea8
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-06-29 00:00:56 +0100

    Add solution to "Count Primes"

---
+++ src/0204-count-primes.cpp
@@ -0,0 +1,24 @@
+// 204. Count Primes
+// https://leetcode.com/problems/count-primes/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int countPrimes(int n) {
+    vector<bool> isPrime(n, true);
+    for (int p = 2; p * p < n; ++p) {
+      if (isPrime[p]) {
+        for (int i = p * p; i < n; i += p)
+          isPrime[i] = false;
+      }
+    }
+    int ans = 0;
+    for (int p = 2; p < n; ++p)
+      if (isPrime[p])
+        ++ans;
+    return ans;
+  }
+};

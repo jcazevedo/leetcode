5eb3800b1c7e0ecdbc05c901255fabe9f1844b2d
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-07-02 00:20:24 +0100

    Add solution to "Reverse Nodes in k-Group"

---
+++ src/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,38 @@
+// 25. Reverse Nodes in k-Group
+// https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode* reverseKGroup(ListNode* head, int k) {
+    if (head == nullptr)
+      return head;
+    ListNode *curr = head, *next = nullptr, *prev = nullptr;
+    int count = 0;
+    while (curr != nullptr && count < k) {
+      curr = curr->next;
+      ++count;
+    }
+    if (count != k)
+      return head;
+    count = 0;
+    curr = head;
+    while (curr != nullptr && count < k) {
+      next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+      ++count;
+    }
+    if (next != nullptr)
+      head->next = reverseKGroup(next, k);
+    return prev;
+  }
+};

8d03e44def1fc5d52dec9e030fda69960bb65fe9
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-07-07 01:01:09 +0100

    Add solution to "Minimum Size Subarray Sum"

---
+++ src/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,24 @@
+// 209. Minimum Size Subarray Sum
+// https://leetcode.com/problems/minimum-size-subarray-sum/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int minSubArrayLen(int target, vector<int>& nums) {
+    int ans = 0, l = 0, sum = 0;
+    for (int r = 0; r < (int)nums.size(); ++r) {
+      sum += nums[r];
+      while (l <= r && (sum - nums[l]) >= target) {
+        sum -= nums[l];
+        ++l;
+      }
+      if (sum >= target)
+        ans = ans == 0 ? (r - l) + 1 : min(ans, (r - l) + 1);
+    }
+    return ans;
+  }
+};

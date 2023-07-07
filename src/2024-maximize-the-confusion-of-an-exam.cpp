0d3e9282b3185fb396f08ba8223434ed0f810262
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-07-07 01:12:13 +0100

    Add solution to "Maximize the Confusion of an Exam"

---
+++ src/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,35 @@
+// 2024. Maximize the Confusion of an Exam
+// https://leetcode.com/problems/maximize-the-confusion-of-an-exam/
+
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxConsecutiveAnswers(string answerKey, int k) {
+    int ans = k, ts = 0, fs = 0, l = 0;
+    for (int i = 0; i < k; ++i) {
+      if (answerKey[i] == 'T')
+        ++ts;
+      if (answerKey[i] == 'F')
+        ++fs;
+    }
+    for (int r = k; r < (int)answerKey.size(); ++r) {
+      if (answerKey[r] == 'T')
+        ++ts;
+      if (answerKey[r] == 'F')
+        ++fs;
+      while (l <= r && min(ts, fs) > k) {
+        if (answerKey[l] == 'T')
+          --ts;
+        if (answerKey[l] == 'F')
+          --fs;
+        ++l;
+      }
+      ans = max(ans, r - l + 1);
+    }
+    return ans;
+  }
+};

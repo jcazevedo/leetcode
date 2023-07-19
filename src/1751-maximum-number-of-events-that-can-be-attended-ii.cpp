62b362dfc62ccd2f65065fde364dc996d02cb3b9
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-07-20 00:28:06 +0100

    Add solution to "Maximum Number of Events That Can Be Attended II"

---
+++ src/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,41 @@
+// 1751. Maximum Number of Events That Can Be Attended II
+// https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended-ii/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+  int binarySearch(const vector<vector<int>>& events, int endTime) {
+    int lo = 0, hi = events.size();
+    while (lo < hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (events[mid][0] <= endTime) {
+        lo = mid + 1;
+      } else {
+        hi = mid;
+      }
+    }
+    return lo;
+  }
+
+  int dfs(int curr, const vector<vector<int>>& events, vector<vector<int>>& cache, int k) {
+    if (curr >= (int)events.size() || k == 0) {
+      return 0;
+    }
+    if (cache[curr][k] == -1) {
+      int next = binarySearch(events, events[curr][1]);
+      cache[curr][k] = max(dfs(curr + 1, events, cache, k), events[curr][2] + dfs(next, events, cache, k - 1));
+    }
+    return cache[curr][k];
+  }
+
+public:
+  int maxValue(vector<vector<int>>& events, int k) {
+    sort(events.begin(), events.end());
+    vector<vector<int>> cache(events.size(), vector<int>(k + 1, -1));
+    return dfs(0, events, cache, k);
+  }
+};

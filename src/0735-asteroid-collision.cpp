22b9eb4eb2df0bdd6f870ba00a9705bf3a8d74e1
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-07-20 09:21:47 +0100

    Add solution to "Asteroid Collision"

---
+++ src/0735-asteroid-collision.cpp
@@ -0,0 +1,39 @@
+// 735. Asteroid Collision
+// https://leetcode.com/problems/asteroid-collision/description/
+
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> asteroidCollision(vector<int>& asteroids) {
+    stack<int> s;
+    for (int a : asteroids) {
+      bool survived = true;
+      while (!s.empty() && (s.top() > 0 && a < 0)) {
+        if (abs(s.top()) < abs(a)) {
+          s.pop();
+          continue;
+        } else if (abs(s.top()) == abs(a)) {
+          s.pop();
+        }
+        survived = false;
+        break;
+      }
+      if (survived) {
+        s.push(a);
+      }
+    }
+    vector<int> ans;
+    while (!s.empty()) {
+      ans.push_back(s.top());
+      s.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+  }
+};

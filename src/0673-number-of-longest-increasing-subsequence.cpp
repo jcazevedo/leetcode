4b22178ddb0c245cc8c90a84098c388ac0369780
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-07-21 16:59:26 +0100

    Add solution to "Number of Longest Increasing Subsequence"

---
+++ src/0673-number-of-longest-increasing-subsequence.cpp
@@ -0,0 +1,39 @@
+// 673. Number of Longest Increasing Subsequence
+// https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int findNumberOfLIS(vector<int>& nums) {
+    int N = nums.size();
+    vector<int> count(N, 1);
+    vector<int> length(N, 1);
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < i; ++j) {
+        if (nums[j] < nums[i]) {
+          if (length[j] + 1 > length[i]) {
+            length[i] = length[j] + 1;
+            count[i] = count[j];
+          } else if (length[j] + 1 == length[i]) {
+            count[i] += count[j];
+          }
+        }
+      }
+    }
+    int maxLength = 0;
+    for (int i = 0; i < N; ++i) {
+      maxLength = max(length[i], maxLength);
+    }
+    int ans = 0;
+    for (int i = 0; i < N; ++i) {
+      if (length[i] == maxLength) {
+        ans += count[i];
+      }
+    }
+    return ans;
+  }
+};

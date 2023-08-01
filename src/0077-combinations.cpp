78bc8ae009bcdd9b52a262ef154bca4c38ce1186
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-08-01 09:50:10 +0100

    Add solution to "Combinations"

---
+++ src/0077-combinations.cpp
@@ -0,0 +1,25 @@
+// 77. Combinations
+// https://leetcode.com/problems/combinations/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> ans;
+    for (int i = 1; i < 1 << n; ++i) {
+      if (__builtin_popcount(i) == k) {
+        vector<int> combination;
+        for (int j = 0; j < n; ++j) {
+          if (i & (1 << j)) {
+            combination.push_back(j + 1);
+          }
+        }
+        ans.push_back(combination);
+      }
+    }
+    return ans;
+  }
+};

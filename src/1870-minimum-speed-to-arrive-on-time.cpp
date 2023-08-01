798301104deddfb8c1bb195f4f3c685744d1c782
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-08-01 23:08:30 +0100

    Add solution to "Minimum Speed to Arrive on Time"

---
+++ src/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,37 @@
+// 1870. Minimum Speed to Arrive on Time
+// https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+  double getTime(const vector<int>& dist, int speed) {
+    double ans = 0;
+    for (unsigned int i = 0; i < dist.size(); ++i) {
+      if (i + 1 < dist.size()) {
+        ans += (dist[i] + speed - 1) / speed;
+      } else {
+        ans += (double)dist[i] / speed;
+      }
+    }
+    return ans;
+  }
+
+public:
+  int minSpeedOnTime(vector<int>& dist, double hour) {
+    int lo = 1, hi = 1e7;
+    while (lo < hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (getTime(dist, mid) <= hour) {
+        hi = mid;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    if (getTime(dist, lo) <= hour)
+      return lo;
+    return -1;
+  }
+};

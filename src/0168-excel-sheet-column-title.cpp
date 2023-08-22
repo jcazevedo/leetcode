f9cefe408a9861771129edc72238e057f2c5e058
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-08-22 01:55:49 +0100

    Add solution to "Excel Sheet Column Title"

---
+++ src/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,19 @@
+// 168. Excel Sheet Column Title
+// https://leetcode.com/problems/excel-sheet-column-title/
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  string convertToTitle(int columnNumber) {
+    string ans = "";
+    while (columnNumber) {
+      columnNumber--;
+      ans = ((char)((columnNumber % 26) + 'A')) + ans;
+      columnNumber /= 26;
+    }
+    return ans;
+  }
+};

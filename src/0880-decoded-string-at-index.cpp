2a6bf19f17e315afafbc87b5cc886bd24dbcbd81
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-09-28 00:26:27 +0100

    Add solution to "Decoded String at Index"

---
+++ src/0880-decoded-string-at-index.cpp
@@ -0,0 +1,36 @@
+// 880. Decoded String at Index
+// https://leetcode.com/problems/decoded-string-at-index/
+
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  string decodeAtIndex(string s, int k) {
+    int N = s.size();
+    long long L = 0;
+
+    for (int i = 0; i < N; ++i)
+      if (isalpha(s[i]))
+        L += 1;
+      else
+        L *= (s[i] - '0');
+
+    for (int i = N - 1; i >= 0; --i) {
+      k = k % L;
+
+      if (k == 0 && isalpha(s[i]))
+        return s.substr(i, 1);
+
+      if (isalpha(s[i]))
+        L -= 1;
+      else {
+        L /= (s[i] - '0');
+      }
+    }
+
+    return "";
+  }
+};

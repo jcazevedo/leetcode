08a6d9129406fc5a83b320ceb0850540dafa9d76
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-10-24 23:58:39 +0100

    Remove unnecessary braces

--- src/0673-number-of-longest-increasing-subsequence.cpp
+++ src/0673-number-of-longest-increasing-subsequence.cpp
@@ -12,28 +12,22 @@ public:
     int N = nums.size();
     vector<int> count(N, 1);
     vector<int> length(N, 1);
-    for (int i = 0; i < N; ++i) {
-      for (int j = 0; j < i; ++j) {
+    for (int i = 0; i < N; ++i)
+      for (int j = 0; j < i; ++j)
         if (nums[j] < nums[i]) {
           if (length[j] + 1 > length[i]) {
             length[i] = length[j] + 1;
             count[i] = count[j];
-          } else if (length[j] + 1 == length[i]) {
+          } else if (length[j] + 1 == length[i])
             count[i] += count[j];
-          }
         }
-      }
-    }
     int maxLength = 0;
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
       maxLength = max(length[i], maxLength);
-    }
     int ans = 0;
-    for (int i = 0; i < N; ++i) {
-      if (length[i] == maxLength) {
+    for (int i = 0; i < N; ++i)
+      if (length[i] == maxLength)
         ans += count[i];
-      }
-    }
     return ans;
   }
 };

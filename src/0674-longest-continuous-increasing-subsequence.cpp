a02db3c9bd62460282093d66bbb0206711a78670
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-10-25 00:01:53 +0100

    Add solution to "Longest Continuous Increasing Subsequence"

---
+++ src/0674-longest-continuous-increasing-subsequence.cpp
@@ -0,0 +1,22 @@
+// 674. Longest Continuous Increasing Subsequence
+// https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int findLengthOfLCIS(vector<int>& nums) {
+    int N = nums.size(), ans = 1, curr = 1;
+    for (int i = 1; i < N; ++i) {
+      if (nums[i] > nums[i - 1])
+        ++curr;
+      else
+        curr = 1;
+      ans = max(ans, curr);
+    }
+    return ans;
+  }
+};

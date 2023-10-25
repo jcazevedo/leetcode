ec75a799a06a1ff24410b91de72c2a2fb9e77d98
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-10-25 10:38:14 +0100

    Remove unnecessary variable

--- src/0730-count-different-palindromic-subsequences.cpp
+++ src/0730-count-different-palindromic-subsequences.cpp
@@ -14,7 +14,7 @@ private:
 
   int mod_sub(int a, int b) { return ((long long)a + MOD - b) % MOD; }
 
-  int go(int i, int j, int ch, int N, const string& s, vector<vector<vector<int>>>& dp) {
+  int go(int i, int j, int ch, const string& s, vector<vector<vector<int>>>& dp) {
     if (i > j)
       return 0;
     if (i == j) {
@@ -28,11 +28,11 @@ private:
     if (s[i] == s[j] && s[j] == (ch + 'a')) {
       dp[i][j][ch] = 2;
       for (int nch = 0; nch < 4; ++nch)
-        dp[i][j][ch] = mod_add(dp[i][j][ch], go(i + 1, j - 1, nch, N, s, dp));
+        dp[i][j][ch] = mod_add(dp[i][j][ch], go(i + 1, j - 1, nch, s, dp));
     } else {
-      dp[i][j][ch] = mod_add(dp[i][j][ch], go(i + 1, j, ch, N, s, dp));
-      dp[i][j][ch] = mod_add(dp[i][j][ch], go(i, j - 1, ch, N, s, dp));
-      dp[i][j][ch] = mod_sub(dp[i][j][ch], go(i + 1, j - 1, ch, N, s, dp));
+      dp[i][j][ch] = mod_add(dp[i][j][ch], go(i + 1, j, ch, s, dp));
+      dp[i][j][ch] = mod_add(dp[i][j][ch], go(i, j - 1, ch, s, dp));
+      dp[i][j][ch] = mod_sub(dp[i][j][ch], go(i + 1, j - 1, ch, s, dp));
     }
     return dp[i][j][ch];
   }
@@ -43,7 +43,7 @@ public:
     vector<vector<vector<int>>> dp(N, vector<vector<int>>(N, vector<int>(4, -1)));
     int ans = 0;
     for (int i = 0; i < 4; ++i)
-      ans = mod_add(ans, go(0, N - 1, i, N, s, dp));
+      ans = mod_add(ans, go(0, N - 1, i, s, dp));
     return ans;
   }
 };

141f921e9989b3e23ec7cf92e89b078939246af0
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-11-04 15:52:30 +0000

    Add solution to "Last Moment Before All Ants Fall Out of a Plank"

---
+++ src/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -0,0 +1,19 @@
+// 1503. Last Moment Before All Ants Fall Out of a Plank
+// https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+    int ans = 0;
+    for (int ant : left)
+      ans = max(ans, ant);
+    for (int ant : right)
+      ans = max(ans, n - ant);
+    return ans;
+  }
+};

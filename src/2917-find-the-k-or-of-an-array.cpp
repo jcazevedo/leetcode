74df77bf05715f7e2bc64f46ad2860206530110f
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-11-04 16:09:45 +0000

    Add solution to "Find the K-or of an Array"

---
+++ src/2917-find-the-k-or-of-an-array.cpp
@@ -0,0 +1,22 @@
+// 2917. Find the K-or of an Array
+// https://leetcode.com/problems/find-the-k-or-of-an-array/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int findKOr(vector<int>& nums, int k) {
+    vector<int> cnt(31, 0);
+    for (int i = 0; i < 31; ++i)
+      for (int num : nums)
+        if ((1 << i) & num)
+          cnt[i]++;
+    int ans = 0;
+    for (int i = 0; i < 31; ++i)
+      if (cnt[i] >= k)
+        ans |= (1 << i);
+    return ans;
+  }
+};

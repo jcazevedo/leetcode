b5106246e6f80c592a368fd21bbc52eca6f5d277
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-11-05 19:52:03 +0000

    Add solution to "Find Champion I"

---
+++ src/2923-find-champion-i.cpp
@@ -0,0 +1,25 @@
+// 2923. Find Champion I
+// https://leetcode.com/problems/find-champion-i/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int findChampion(vector<vector<int>>& grid) {
+    int N = grid.size();
+    for (int i = 0; i < N; ++i) {
+      int j = 0;
+      for (; j < N; ++j) {
+        if (i == j)
+          continue;
+        if (grid[i][j] != 1)
+          break;
+      }
+      if (j == N)
+        return i;
+    }
+    return -1;
+  }
+};

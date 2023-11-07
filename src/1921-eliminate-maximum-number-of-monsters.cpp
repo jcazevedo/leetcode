9b5b6977836f3727e8a07ae607aabeec294dd069
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-11-07 01:51:34 +0000

    Add solution to "Eliminate Maximum Number of Monsters"

---
+++ src/1921-eliminate-maximum-number-of-monsters.cpp
@@ -0,0 +1,25 @@
+// 1921. Eliminate Maximum Number of Monsters
+// https://leetcode.com/problems/eliminate-maximum-number-of-monsters/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+    int N = dist.size();
+    vector<int> turnsToReachCity(N, 0);
+    for (int i = 0; i < N; ++i)
+      turnsToReachCity[i] = (dist[i] + speed[i] - 1) / speed[i];
+    sort(turnsToReachCity.begin(), turnsToReachCity.end());
+    int ans = 0;
+    for (int i = 0; i < N; ++i) {
+      if (turnsToReachCity[i] <= ans)
+        break;
+      ++ans;
+    }
+    return ans;
+  }
+};

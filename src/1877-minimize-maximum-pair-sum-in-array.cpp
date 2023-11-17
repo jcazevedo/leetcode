72f585e8b4d98a1f54df0ca3640ffffa0be9bfaf
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-11-17 23:35:58 +0000

    Add solution to "Minimize Maximum Pair Sum in Array"

---
+++ src/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,19 @@
+// 1877. Minimize Maximum Pair Sum in Array
+// https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int minPairSum(vector<int>& nums) {
+    int N = nums.size();
+    sort(nums.begin(), nums.end());
+    int ans = 0;
+    for (int i = 0; i < N / 2; ++i)
+      ans = max(ans, nums[i] + nums[N - i - 1]);
+    return ans;
+  }
+};

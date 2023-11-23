041bfc5d95f063296e934013c2d3483a117538f4
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-11-23 22:43:27 +0000

    Add solution to "Next Permutation"

---
+++ src/0031-next-permutation.cpp
@@ -0,0 +1,24 @@
+// 31. Next Permutation
+// https://leetcode.com/problems/next-permutation/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void nextPermutation(vector<int>& nums) {
+    int N = nums.size(), i = N - 2, j = N - 1;
+    while (i >= 0 && nums[i] >= nums[i + 1])
+      --i;
+    if (i < 0)
+      reverse(nums.begin(), nums.end());
+    else {
+      while (j > i && nums[j] <= nums[i])
+        --j;
+      swap(nums[i], nums[j]);
+      reverse(begin(nums) + i + 1, end(nums));
+    }
+  }
+};

f812056c20bab0b7ca0c8a412b134d9f8e658cbe
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-11-24 10:29:32 +0000

    Add solution to "Maximum Number of Coins You Can Get"

---
+++ src/1561-maximum-number-of-coins-you-can-get.cpp
@@ -0,0 +1,21 @@
+// 1561. Maximum Number of Coins You Can Get
+// https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxCoins(vector<int>& piles) {
+    sort(piles.begin(), piles.end());
+    int N = piles.size(), bob = 0, you = N - 2, ans = 0;
+    while (bob < you) {
+      ans += piles[you];
+      you -= 2;
+      bob += 1;
+    }
+    return ans;
+  }
+};

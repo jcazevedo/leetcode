52a246d60ac42a2da045ad676b2c532541c90d1c
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-01-10 18:39:03 +0000

    Add solution to "Leaf-Similar Trees"

---
+++ src/0872-leaf-similar-trees.cpp
@@ -0,0 +1,34 @@
+// 872. Leaf-Similar Trees
+// https://leetcode.com/problems/leaf-similar-trees/
+
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ private:
+  void leafValueSequence(TreeNode* root, vector<int>& curr) {
+    if (root == nullptr || (root->left == nullptr && root->right == nullptr))
+      curr.push_back(root->val);
+    if (root->left != nullptr) leafValueSequence(root->left, curr);
+    if (root->right != nullptr) leafValueSequence(root->right, curr);
+  }
+
+ public:
+  bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+    vector<int> v1, v2;
+    leafValueSequence(root1, v1);
+    leafValueSequence(root2, v2);
+    return v1 == v2;
+  }
+};

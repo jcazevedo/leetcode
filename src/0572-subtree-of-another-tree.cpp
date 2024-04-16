25d05d8a6dcd1d90987845c33f4d6fea332d8b08
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-04-16 22:34:15 +0100

    Add solution to "Subtree of Another Tree"

---
+++ src/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,33 @@
+// 572. Subtree of Another Tree
+// https://leetcode.com/problems/subtree-of-another-tree/
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  bool isSubtree(TreeNode *root, TreeNode *subRoot, bool inSubtree = false) {
+    if (root == nullptr && subRoot != nullptr) return false;
+
+    if (root != nullptr && subRoot == nullptr) return false;
+
+    if (root == nullptr && subRoot == nullptr) return true;
+
+    bool currentSubTree = false;
+    if (root->val == subRoot->val)
+      currentSubTree = isSubtree(root->left, subRoot->left, true) &&
+                       isSubtree(root->right, subRoot->right, true);
+    if (currentSubTree) return true;
+    if (inSubtree) return false;
+
+    return isSubtree(root->left, subRoot, false) ||
+           isSubtree(root->right, subRoot, false);
+  }
+};

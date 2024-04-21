1c446d59989fccc20246aea4d35ec7ec46f1e7b8
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-04-22 00:51:04 +0100

    Add solution to "First Day Where You Have Been in All the Rooms"

---
+++ src/1997-first-day-where-you-have-been-in-all-the-rooms.cpp
@@ -0,0 +1,21 @@
+// 1997. First Day Where You Have Been in All the Rooms
+// https://leetcode.com/problems/first-day-where-you-have-been-in-all-the-rooms/
+
+#include <vector>
+
+using namespace std;
+
+#define MOD 1000000007
+
+class Solution {
+ public:
+  int firstDayBeenInAllRooms(vector<int>& nextVisit) {
+    int N = nextVisit.size();
+    vector<long long> dp(N, -1L);
+    dp[0] = 0L;
+    for (int i = 1; i < N; ++i) {
+      dp[i] = ((2L * dp[i - 1]) % MOD + MOD - dp[nextVisit[i - 1]] + 2L) % MOD;
+    }
+    return dp[N - 1];
+  }
+};

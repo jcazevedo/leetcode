a1b143b54f163ea2566e2007af02677d9616f314
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-04-30 16:53:21 +0100

    Improve solution to "Number of Wonderful Substrings"

--- src/1915-number-of-wonderful-substrings.cpp
+++ src/1915-number-of-wonderful-substrings.cpp
@@ -1,23 +1,22 @@
 // 1915. Number of Wonderful Substrings
 // https://leetcode.com/problems/number-of-wonderful-substrings/
 
+#include <cstring>
 #include <string>
-#include <unordered_map>
 
 using namespace std;
 
 class Solution {
  public:
   long long wonderfulSubstrings(string word) {
-    unordered_map<int, long long> prev;
-    int N = word.size(), curr = 0;
-    long long ans = 0;
+    int N = word.size(), curr = 0, len = 'j' - 'a' + 1;
+    long long ans = 0, prev[1 << len];
+    memset(prev, 0, sizeof(prev));
     prev[0] = 1L;
     for (int i = 0; i < N; ++i) {
       curr ^= (1 << (word[i] - 'a'));
       ans += prev[curr];
-      for (char ch = 'a'; ch <= 'j'; ++ch)
-        ans += prev[curr ^ (1 << (ch - 'a'))];
+      for (int d = 0; d < len; ++d) ans += prev[curr ^ (1 << d)];
       ++prev[curr];
     }
     return ans;

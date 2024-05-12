c097eb3864ce60840841971e95df66fefc099bdf
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-05-12 17:11:44 +0100

    Improve solution to "Largest Local Values in a Matrix"

--- src/2373-largest-local-values-in-a-matrix.cpp
+++ src/2373-largest-local-values-in-a-matrix.cpp
@@ -1,7 +1,6 @@
 // 2373. Largest Local Values in a Matrix
 // https://leetcode.com/problems/largest-local-values-in-a-matrix/
 
-#include <queue>
 #include <vector>
 
 using namespace std;
@@ -12,12 +11,10 @@ class Solution {
     int N = grid.size();
     vector<vector<int>> ans = vector<vector<int>>(N - 2, vector<int>(N - 2, 0));
     for (int i = 1; i <= N - 2; ++i)
-      for (int j = 1; j <= N - 2; ++j) {
-        priority_queue<int> pq;
+      for (int j = 1; j <= N - 2; ++j)
         for (int di = -1; di <= 1; ++di)
-          for (int dj = -1; dj <= 1; ++dj) pq.push(grid[i + di][j + dj]);
-        ans[i - 1][j - 1] = pq.top();
-      }
+          for (int dj = -1; dj <= 1; ++dj)
+            ans[i - 1][j - 1] = max(ans[i - 1][j - 1], grid[i + di][j + dj]);
     return ans;
   }
 };

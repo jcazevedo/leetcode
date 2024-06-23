0098c7a7d45c04e8b43495c63194e63c44944fab
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-06-23 12:26:58 +0100

    Improve solution to "Maximum Score of a Good Subarray"

--- src/1793-maximum-score-of-a-good-subarray.cpp
+++ src/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,7 +1,6 @@
 // 1793. Maximum Score of a Good Subarray
 // https://leetcode.com/problems/maximum-score-of-a-good-subarray/
 
-#include <stack>
 #include <vector>
 
 using namespace std;
@@ -10,25 +9,21 @@ class Solution {
  public:
   int maximumScore(vector<int>& nums, int k) {
     int n = nums.size();
-    vector<int> right(n, 0);
-    vector<int> left(n, 0);
-    stack<int> s;
-    s.push(n);
-    for (int i = n - 1; i >= 0; i--) {
-      while (s.top() != n && nums[s.top()] >= nums[i]) { s.pop(); }
-      right[i] = s.top();
-      s.push(i);
-    }
-    while (!s.empty()) { s.pop(); }
-    s.push(-1);
-    for (int i = 0; i < n; ++i) {
-      while (s.top() != -1 && nums[s.top()] >= nums[i]) { s.pop(); }
-      left[i] = s.top();
-      s.push(i);
-    }
-    int ans = 0;
-    for (int i = 0; i < n; ++i) {
-      if (k < right[i] && k > left[i]) { ans = max(ans, nums[i] * (right[i] - left[i] - 1)); }
+    int left = k;
+    int right = k;
+    int ans = nums[k];
+    int minV = nums[k];
+    while (left > 0 || right < n - 1) {
+      int nextLeft = left > 0 ? nums[left - 1] : 0;
+      int nextRight = right < n - 1 ? nums[right + 1] : 0;
+      if (nextLeft < nextRight) {
+        ++right;
+        minV = min(minV, nums[right]);
+      } else {
+        --left;
+        minV = min(minV, nums[left]);
+      }
+      ans = max(ans, minV * (right - left + 1));
     }
     return ans;
   }

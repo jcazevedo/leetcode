77e5a093d430343a1f23a3ce9e9aa165ad37429d
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-07-24 01:07:40 +0100

    Add solution to "Sort the Jumbled Numbers"

---
+++ src/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,34 @@
+// 2191. Sort the Jumbled Numbers
+// https://leetcode.com/problems/sort-the-jumbled-numbers/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ private:
+  int mappedValue(int num, const vector<int>& mapping) {
+    if (num == 0) return mapping[0];
+    int ans = 0;
+    int p = 1;
+    while (num) {
+      int dig = mapping[num % 10];
+      ans += p * dig;
+      p *= 10;
+      num /= 10;
+    }
+    return ans;
+  }
+
+ public:
+  vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+    vector<vector<int>> toSort;
+    int N = nums.size();
+    for (int i = 0; i < N; ++i) { toSort.push_back({mappedValue(nums[i], mapping), i}); }
+    sort(toSort.begin(), toSort.end());
+    vector<int> ans;
+    for (const vector<int>& v : toSort) { ans.push_back(nums[v[1]]); }
+    return ans;
+  }
+};

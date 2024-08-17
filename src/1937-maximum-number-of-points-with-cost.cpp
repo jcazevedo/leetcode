95f4aef610399286788bd73067800a2f9d2a4b22
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-08-17 15:38:58 +0100

    Improve solution to Maximum Number of Points with Cost

--- src/1937-maximum-number-of-points-with-cost.cpp
+++ src/1937-maximum-number-of-points-with-cost.cpp
@@ -10,14 +10,12 @@ class Solution {
  public:
   long long maxPoints(vector<vector<int>>& points) {
     int R = points.size(), C = points[0].size();
-    vector<long long> prev(C), left(C), right(C);
+    vector<long long> prev(C);
     for (int c = 0; c < C; ++c) { prev[c] = points[0][c]; }
     for (int r = 1; r < R; ++r) {
-      left[0] = prev[0];
-      for (int c = 1; c < C; ++c) { left[c] = max(left[c - 1] - 1, prev[c]); }
-      right[C - 1] = prev[C - 1];
-      for (int c = C - 2; c >= 0; --c) { right[c] = max(right[c + 1] - 1, prev[c]); }
-      for (int c = 0; c < C; ++c) { prev[c] = max(left[c], right[c]) + points[r][c]; }
+      for (int c = 1; c < C; ++c) { prev[c] = max(prev[c - 1] - 1, prev[c]); }
+      for (int c = C - 2; c >= 0; --c) { prev[c] = max(prev[c + 1] - 1, prev[c]); }
+      for (int c = 0; c < C; ++c) { prev[c] += points[r][c]; }
     }
     long long ans = prev[0];
     for (int c = 1; c < C; ++c) { ans = max(ans, prev[c]); }

b855fe0b2d97208863cc2c9b128abc3a9ba9bdc9
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-08-19 01:31:43 +0100

    Prefer M and N to R and C

--- src/1937-maximum-number-of-points-with-cost.cpp
+++ src/1937-maximum-number-of-points-with-cost.cpp
@@ -9,16 +9,16 @@ using namespace std;
 class Solution {
  public:
   long long maxPoints(vector<vector<int>>& points) {
-    int R = points.size(), C = points[0].size();
-    vector<long long> prev(C);
-    for (int c = 0; c < C; ++c) { prev[c] = points[0][c]; }
-    for (int r = 1; r < R; ++r) {
-      for (int c = 1; c < C; ++c) { prev[c] = max(prev[c - 1] - 1, prev[c]); }
-      for (int c = C - 2; c >= 0; --c) { prev[c] = max(prev[c + 1] - 1, prev[c]); }
-      for (int c = 0; c < C; ++c) { prev[c] += points[r][c]; }
+    int M = points.size(), N = points[0].size();
+    vector<long long> prev(N);
+    for (int c = 0; c < N; ++c) { prev[c] = points[0][c]; }
+    for (int r = 1; r < M; ++r) {
+      for (int c = 1; c < N; ++c) { prev[c] = max(prev[c - 1] - 1, prev[c]); }
+      for (int c = N - 2; c >= 0; --c) { prev[c] = max(prev[c + 1] - 1, prev[c]); }
+      for (int c = 0; c < N; ++c) { prev[c] += points[r][c]; }
     }
     long long ans = prev[0];
-    for (int c = 1; c < C; ++c) { ans = max(ans, prev[c]); }
+    for (int c = 1; c < N; ++c) { ans = max(ans, prev[c]); }
     return ans;
   }
 };

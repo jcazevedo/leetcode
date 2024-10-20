10e56fe800fdeb98b252fd509f1d72d37c0077f1
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-10-20 12:39:31 +0100

    Prefer a switch statement

--- src/1106-parsing-a-boolean-expression.cpp
+++ src/1106-parsing-a-boolean-expression.cpp
@@ -14,36 +14,44 @@ class Solution {
     int N = expression.size();
     int i = 0;
     while (i < N) {
-      if (expression[i] == 't') {
-        arg.push(true);
-      } else if (expression[i] == 'f') {
-        arg.push(false);
-      } else if (expression[i] == '!') {
-        op.push('!');
-        ++i;
-      } else if (expression[i] == '&') {
-        op.push('&');
-        arg.push(true);
-        ++i;
-      } else if (expression[i] == '|') {
-        op.push('|');
-        arg.push(false);
-        ++i;
-      } else if (expression[i] == ',' || expression[i] == ')') {
-        bool arg1 = arg.top();
-        arg.pop();
-        if (op.top() == '!') {
-          arg.push(!arg1);
-        } else {
-          bool arg2 = arg.top();
+      switch (expression[i]) {
+        case 't':
+          arg.push(true);
+          break;
+        case 'f':
+          arg.push(false);
+          break;
+        case '!':
+          op.push('!');
+          ++i;
+          break;
+        case '&':
+          op.push('&');
+          arg.push(true);
+          ++i;
+          break;
+        case '|':
+          op.push('|');
+          arg.push(false);
+          ++i;
+          break;
+        case ',':
+        case ')':
+          bool arg1 = arg.top();
           arg.pop();
-          if (op.top() == '&') {
-            arg.push(arg1 && arg2);
-          } else if (op.top() == '|') {
-            arg.push(arg1 || arg2);
+          if (op.top() == '!') {
+            arg.push(!arg1);
+          } else {
+            bool arg2 = arg.top();
+            arg.pop();
+            if (op.top() == '&') {
+              arg.push(arg1 && arg2);
+            } else if (op.top() == '|') {
+              arg.push(arg1 || arg2);
+            }
           }
-        }
-        if (expression[i] == ')') { op.pop(); }
+          if (expression[i] == ')') { op.pop(); }
+          break;
       }
       ++i;
     }

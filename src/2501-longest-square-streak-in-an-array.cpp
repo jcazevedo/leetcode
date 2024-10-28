bc06d42693b092e0cb1d9792b163ab8c68b7ae97
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-10-28 09:20:00 +0000

    Improve solution to Longest Square Streak in an Array

--- src/2501-longest-square-streak-in-an-array.cpp
+++ src/2501-longest-square-streak-in-an-array.cpp
@@ -3,7 +3,6 @@
 
 #include <algorithm>
 #include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -11,22 +10,16 @@ using namespace std;
 class Solution {
  public:
   int longestSquareStreak(vector<int>& nums) {
-    unordered_set<long long> numSet;
-    for (int num : nums) { numSet.insert(num); }
     int N = nums.size();
     sort(begin(nums), end(nums));
     unordered_map<long long, int> len;
     int ans = -1;
     for (int i = N - 1; i >= 0; --i) {
       long long sq = ((long long)nums[i]) * nums[i];
-      if (numSet.find(sq) != numSet.end()) {
-        int l = len[sq] + 1;
-        len[nums[i]] = l;
-        ans = max(ans, l);
-      } else {
-        len[nums[i]] = 1;
-      }
+      int l = len[sq] + 1;
+      len[nums[i]] = l;
+      ans = max(ans, l);
     }
-    return ans;
+    return ans > 1 ? ans : -1;
   }
 };

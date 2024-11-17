7e3b8fabc3284d9bd8753238fca7aee0d4ed1117
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-11-17 12:33:35 +0000

    Improve solution to Shortest Subarray with Sum at Least K

--- src/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ src/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,6 +1,7 @@
 // 862. Shortest Subarray with Sum at Least K
 // https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
 
+#include <limits>
 #include <queue>
 #include <vector>
 
@@ -10,28 +11,18 @@ class Solution {
  public:
   int shortestSubarray(vector<int>& nums, int k) {
     int N = nums.size();
-    int ans = -1;
+    int ans = numeric_limits<int>::max();
     long long curr = 0L;
     priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
     for (int i = 0; i < N; ++i) {
       curr += nums[i];
-      if (curr >= k) {
-        if (ans == -1) {
-          ans = i + 1;
-        } else {
-          ans = min(ans, i + 1);
-        }
-      }
+      if (curr >= k) { ans = min(ans, i + 1); }
       while (!pq.empty() && curr - pq.top().first >= k) {
-        if (ans == -1) {
-          ans = i - pq.top().second;
-        } else {
-          ans = min(ans, i - pq.top().second);
-        }
+        ans = min(ans, i - pq.top().second);
         pq.pop();
       }
       pq.emplace(curr, i);
     }
-    return ans;
+    return ans == numeric_limits<int>::max() ? -1 : ans;
   }
 };

174f8b917f285701cd07d96e6aad7981f56db12a
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-11-26 00:33:52 +0000

    Reformat solution to Find Champion II

--- src/2924-find-champion-ii.cpp
+++ src/2924-find-champion-ii.cpp
@@ -9,13 +9,14 @@ class Solution {
  public:
   int findChampion(int n, vector<vector<int>>& edges) {
     vector<int> inDeg(n, 0);
-    for (const vector<int>& edge : edges) inDeg[edge[1]]++;
+    for (const vector<int>& edge : edges) { inDeg[edge[1]]++; }
     int ans = -1;
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i) {
       if (inDeg[i] == 0) {
-        if (ans != -1) return -1;
+        if (ans != -1) { return -1; }
         ans = i;
       }
+    }
     return ans;
   }
 };

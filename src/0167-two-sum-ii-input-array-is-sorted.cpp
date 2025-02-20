ed1cf6ff3ab98aeac2dcf0ed2732730a780cb758
Joao Azevedo <joao.c.azevedo@gmail.com>
2025-02-20 00:00:23 +0000

    Reformat Two Sum II - Input Array Is Sorted

--- src/0167-two-sum-ii-input-array-is-sorted.cpp
+++ src/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -15,7 +15,7 @@ class Solution {
       int hi = n;
       while (lo < hi) {
         int mid = lo + (hi - lo) / 2;
-        if (numbers[mid] == rem) return {i + 1, mid + 1};
+        if (numbers[mid] == rem) { return {i + 1, mid + 1}; }
         if (numbers[mid] < rem) {
           lo = mid + 1;
         } else {

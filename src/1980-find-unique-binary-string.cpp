829faf1cf6c21db49917c5960959c32ccf7ad9ad
Joao Azevedo <joao.c.azevedo@gmail.com>
2025-02-20 00:01:20 +0000

    Reformat solution to Find Unique Binary String

--- src/1980-find-unique-binary-string.cpp
+++ src/1980-find-unique-binary-string.cpp
@@ -11,23 +11,20 @@ class Solution {
  private:
   string dfs(string curr, int N, const set<string>& nums) {
     if ((int)curr.size() == N) {
-      if (nums.find(curr) == nums.end()) return curr;
+      if (nums.find(curr) == nums.end()) { return curr; }
       return "";
     }
-
     for (char ch = '0'; ch <= '1'; ++ch) {
       string next = dfs(curr + ch, N, nums);
-      if (next != "") return next;
+      if (next != "") { return next; }
     }
-
     return "";
   }
 
  public:
   string findDifferentBinaryString(vector<string>& nums) {
     set<string> numsSet;
-    for (string num : nums) numsSet.insert(num);
-
+    for (string num : nums) { numsSet.insert(num); }
     return dfs("", nums.size(), numsSet);
   }
 };

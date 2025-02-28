a7dd3c417f832c47374bb9a7ee7fbfc90c3ffa9d
Joao Azevedo <joao.c.azevedo@gmail.com>
2025-02-28 00:25:28 +0000

    Reformat solution to Shortest Common Supersequence

--- src/1092-shortest-common-supersequence.cpp
+++ src/1092-shortest-common-supersequence.cpp
@@ -12,12 +12,15 @@ class Solution {
   string shortestCommonSupersequence(string str1, string str2) {
     int N1 = str1.size(), N2 = str2.size();
     vector<vector<int>> dp(N1 + 1, vector<int>(N2 + 1, 0));
-    for (int i1 = 1; i1 <= N1; ++i1)
-      for (int i2 = 1; i2 <= N2; ++i2)
-        if (str1[i1 - 1] == str2[i2 - 1])
+    for (int i1 = 1; i1 <= N1; ++i1) {
+      for (int i2 = 1; i2 <= N2; ++i2) {
+        if (str1[i1 - 1] == str2[i2 - 1]) {
           dp[i1][i2] = dp[i1 - 1][i2 - 1] + 1;
-        else
+        } else {
           dp[i1][i2] = max(dp[i1 - 1][i2], dp[i1][i2 - 1]);
+        }
+      }
+    }
     string ans = "";
     while (N1 > 0 && N2 > 0) {
       if (str1[N1 - 1] == str2[N2 - 1]) {
@@ -32,8 +35,8 @@ class Solution {
         N2--;
       }
     }
-    while (N1 > 0) ans += str1[--N1];
-    while (N2 > 0) ans += str2[--N2];
+    while (N1 > 0) { ans += str1[--N1]; }
+    while (N2 > 0) { ans += str2[--N2]; }
     reverse(ans.begin(), ans.end());
     return ans;
   }

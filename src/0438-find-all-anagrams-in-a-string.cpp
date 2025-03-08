6cb475b4b9fb6bf17e74dde495bc2e5da82c40e8
Joao Azevedo <joao.c.azevedo@gmail.com>
2025-03-08 12:33:09 +0000

    Reformat solution to Find All Anagrams in a String

--- src/0438-find-all-anagrams-in-a-string.cpp
+++ src/0438-find-all-anagrams-in-a-string.cpp
@@ -11,7 +11,7 @@ class Solution {
  private:
   bool is_anagram(map<char, int>& current, map<char, int>& expected) {
     for (auto itr = expected.begin(); itr != expected.end(); ++itr) {
-      if (current[itr->first] != itr->second) return false;
+      if (current[itr->first] != itr->second) { return false; }
     }
     return true;
   }
@@ -19,17 +19,16 @@ class Solution {
  public:
   vector<int> findAnagrams(string s, string p) {
     map<char, int> expected;
-    for (char ch : p) expected[ch]++;
+    for (char ch : p) { expected[ch]++; }
     map<char, int> cnt;
     int S = s.size(), P = p.size();
-    ;
     vector<int> res;
     if (S >= P) {
-      for (int i = 0; i < P; ++i) cnt[s[i]]++;
+      for (int i = 0; i < P; ++i) { cnt[s[i]]++; }
       for (int i = 0; i + P <= S; ++i) {
-        if (is_anagram(cnt, expected)) res.push_back(i);
+        if (is_anagram(cnt, expected)) { res.push_back(i); }
         cnt[s[i]]--;
-        if (i + P < S) cnt[s[i + P]]++;
+        if (i + P < S) { cnt[s[i + P]]++; }
       }
     }
     return res;

d57ec1758c711d9c8bf356169efb05a32b2a1e1c
Joao Azevedo <joao.c.azevedo@gmail.com>
2025-03-31 01:05:03 +0100

    Reformat solution to Put Marbles in Bags

--- src/2551-put-marbles-in-bags.cpp
+++ src/2551-put-marbles-in-bags.cpp
@@ -11,7 +11,7 @@ class Solution {
   long long putMarbles(vector<int>& weights, int k) {
     int N = weights.size();
     vector<long long> splits;
-    for (int i = 0; i < N - 1; ++i) splits.push_back(weights[i] + weights[i + 1]);
+    for (int i = 0; i < N - 1; ++i) { splits.push_back(weights[i] + weights[i + 1]); }
     sort(splits.begin(), splits.end());
     long long maxV, minV;
     maxV = minV = weights[0] + weights[N - 1];

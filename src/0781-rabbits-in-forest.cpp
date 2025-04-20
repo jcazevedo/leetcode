d9e83f34f25c7b305b144ab9b12b1bf7969887d5
Joao Azevedo <joao.c.azevedo@gmail.com>
2025-04-20 11:45:14 +0100

    Improve solution to Rabbits in Forest

--- src/0781-rabbits-in-forest.cpp
+++ src/0781-rabbits-in-forest.cpp
@@ -12,14 +12,7 @@ class Solution {
     unordered_map<int, int> cnt;
     for (int answer : answers) { ++cnt[answer]; }
     int ans = 0;
-    for (const auto& [k, v] : cnt) {
-      int tot = v;
-      while (tot > k + 1) {
-        ans += k + 1;
-        tot -= k + 1;
-      }
-      if (tot) { ans += k + 1; }
-    }
+    for (const auto& [k, v] : cnt) { ans += ((v + k) / (k + 1)) * (k + 1); }
     return ans;
   }
 };

223531421877afe586eab2b2669376ecfb059ef2
Joao Azevedo <joao.c.azevedo@gmail.com>
2025-05-03 13:22:12 +0100

    Reformat solution to Minimum Domino Rotations For Equal Row

--- src/1007-minimum-domino-rotations-for-equal-row.cpp
+++ src/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -12,22 +12,22 @@ class Solution {
     int N = A.size();
     unordered_map<int, int> cnt;
     for (int i = 0; i < N; ++i) {
-      cnt[A[i]]++;
-      if (B[i] != A[i]) cnt[B[i]]++;
+      ++cnt[A[i]];
+      if (B[i] != A[i]) { ++cnt[B[i]]; }
     }
     int best = -1;
     for (int i = 1; i <= 6; ++i) {
-      if (cnt[i] != N) continue;
+      if (cnt[i] != N) { continue; }
       int curr = 0;
       for (int j = 0; j < N; ++j) {
-        if (A[j] != i) curr++;
+        if (A[j] != i) { ++curr; }
       }
-      if (best == -1 || curr < best) best = curr;
+      if (best == -1 || curr < best) { best = curr; }
       curr = 0;
       for (int j = 0; j < N; ++j) {
-        if (B[j] != i) curr++;
+        if (B[j] != i) { ++curr; }
       }
-      if (best == -1 || curr < best) best = curr;
+      if (best == -1 || curr < best) { best = curr; }
     }
     return best;
   }

9f99f0f3fe2b863cb39abec1a79cade914d7d809
Joao Azevedo <joao.c.azevedo@gmail.com>
2025-09-03 17:39:40 +0100

    Add solution to Find the Number of Ways to Place People I

---
+++ src/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,30 @@
+// 3025. Find the Number of Ways to Place People I
+// https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/
+
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int numberOfPairs(vector<vector<int>>& points) {
+    int ans = 0, n = points.size();
+    sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b) {
+      if (a[0] < b[0]) { return true; }
+      if (a[0] == b[0]) { return a[1] > b[1]; }
+      return false;
+    });
+    for (int i = 0; i < n; ++i) {
+      int min_y = numeric_limits<int>::min();
+      for (int j = i + 1; j < n; ++j) {
+        if (points[j][1] > points[i][1]) { continue; }
+        if (points[j][1] <= min_y) { continue; }
+        ++ans;
+        min_y = max(points[j][1], min_y);
+      }
+    }
+    return ans;
+  }
+};

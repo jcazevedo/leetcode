cc10c036e2d972fd550e56e05bf6ff74a3d46819
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-01-16 17:11:30 +0000

    Add solution to Maximum Square Area by Removing Fences From a Field

---
+++ src/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -0,0 +1,38 @@
+// 2975. Maximum Square Area by Removing Fences From a Field
+// https://leetcode.com/problems/maximum-square-area-by-removing-fences-from-a-field/
+
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+    sort(hFences.begin(), hFences.end());
+    hFences.insert(hFences.begin(), 1);
+    hFences.push_back(m);
+    sort(vFences.begin(), vFences.end());
+    vFences.insert(vFences.begin(), 1);
+    vFences.push_back(n);
+    unordered_set<int> hasHFence;
+    for (int hFence : hFences) { hasHFence.insert(hFence); }
+    unordered_set<int> hasVFence;
+    for (int vFence : vFences) { hasVFence.insert(vFence); }
+    int H = hFences.size();
+    int V = vFences.size();
+    long long ans = -1L;
+    unordered_set<int> hasWidth;
+    for (int i = 0; i < V; ++i) {
+      for (int j = i + 1; j < V; ++j) { hasWidth.insert(vFences[j] - vFences[i]); }
+    }
+    for (int i = 0; i < H; ++i) {
+      for (int j = i + 1; j < H; ++j) {
+        long long h = ((long long)hFences[j]) - hFences[i];
+        if (hasWidth.count(h)) { ans = max(ans, h * h); }
+      }
+    }
+    return ans % 1000000007;
+  }
+};

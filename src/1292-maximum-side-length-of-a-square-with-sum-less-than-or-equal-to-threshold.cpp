8aa108d01b45bc5ba19a919b79d389e781daf1e7
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-01-19 00:52:06 +0000

    Add solution to Maximum Side Length of a Square with Sum Less than or Equal to Threshold

---
+++ src/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,37 @@
+// 1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold
+// https://leetcode.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int maxSideLength(vector<vector<int>>& mat, int threshold) {
+    int m = mat.size();
+    int n = mat[0].size();
+    vector<vector<int>> ps(m, vector<int>(n));
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        ps[i][j] = mat[i][j];
+        if (i > 0) { ps[i][j] += ps[i - 1][j]; }
+        if (j > 0) { ps[i][j] += ps[i][j - 1]; }
+        if (i > 0 && j > 0) { ps[i][j] -= ps[i - 1][j - 1]; }
+      }
+    }
+    int ans = 0;
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        for (int l = ans + 1; l <= min(m - i, n - j); ++l) {
+          int sum = ps[i + l - 1][j + l - 1];
+          if (i > 0) { sum -= ps[i - 1][j + l - 1]; }
+          if (j > 0) { sum -= ps[i + l - 1][j - 1]; }
+          if (i > 0 && j > 0) { sum += ps[i - 1][j - 1]; }
+          if (sum <= threshold) { ans = max(ans, l); }
+        }
+      }
+    }
+    return ans;
+  }
+};

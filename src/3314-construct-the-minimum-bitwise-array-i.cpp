2b1b33482553f17c79c6e0b35907e8cf5ff0dd41
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-01-21 21:53:52 +0000

    Improve solution to Construct the Minimum Bitwise Array I

--- src/3314-construct-the-minimum-bitwise-array-i.cpp
+++ src/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -9,18 +9,12 @@ class Solution {
  public:
   vector<int> minBitwiseArray(vector<int>& nums) {
     int n = nums.size();
-    vector<int> ans(n);
+    vector<int> ans(n, -1);
     for (int i = 0; i < n; ++i) {
-      if (nums[i] % 2 == 0) {
-        ans[i] = -1;
-      } else {
-        for (int n = 0; n < nums[i]; ++n) {
-          if ((n | (n + 1)) == nums[i]) {
-            ans[i] = n;
-            break;
-          }
-        }
-      }
+      if (nums[i] % 2 == 0) { continue; }
+      int b = 1;
+      while (nums[i] & (b << 1)) { b <<= 1; }
+      ans[i] = nums[i] - b;
     }
     return ans;
   }

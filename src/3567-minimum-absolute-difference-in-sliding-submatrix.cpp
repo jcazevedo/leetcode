3021503ae49fedf62f7d22d59d7d0678b9329c98
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-22 23:23:23 +0000

    Add solution to Minimum Absolute Difference in Sliding Submatrix

---
+++ src/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -0,0 +1,36 @@
+// 3567. Minimum Absolute Difference in Sliding Submatrix
+// https://leetcode.com/problems/minimum-absolute-difference-in-sliding-submatrix/
+
+#include <algorithm>
+#include <limits>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+    int m = grid.size(), n = grid[0].size();
+    vector<vector<int>> ans = vector<vector<int>>(m - k + 1, vector<int>(n - k + 1, 0));
+    for (int i = 0; i + k <= m; ++i) {
+      for (int j = 0; j + k <= n; ++j) {
+        set<int> values;
+        for (int ii = i; ii < i + k; ++ii) {
+          for (int jj = j; jj < j + k; ++jj) { values.insert(grid[ii][jj]); }
+        }
+        if (values.size() == 1) {
+          ans[i][j] = 0;
+        } else {
+          ans[i][j] = numeric_limits<int>::max();
+          for (auto itr = values.begin(); itr != values.end(); ++itr) {
+            auto itr1 = itr;
+            ++itr1;
+            if (itr1 != values.end()) { ans[i][j] = min(ans[i][j], *itr1 - *itr); }
+          }
+        }
+      }
+    }
+    return ans;
+  }
+};

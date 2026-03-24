592c83b5f55cd7a887004e4b60b9c44da5b4641b
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-24 19:59:10 +0000

    Improve solution to Construct Product Matrix

--- src/2906-construct-product-matrix.cpp
+++ src/2906-construct-product-matrix.cpp
@@ -11,33 +11,21 @@ class Solution {
  public:
   vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
     int n = grid.size(), m = grid[0].size();
-    vector<long long> pre(n * m, 1L), suf(n * m, 1L);
-    pre[0] = grid[0][0] % MOD;
-    for (int i = 0; i < n; ++i) {
-      for (int j = i == 0 ? 1 : 0; j < m; ++j) {
-        int ii = i * m + j;
-        pre[ii] = (pre[ii - 1] * grid[i][j]) % MOD;
-      }
-    }
-    suf[n * m - 1] = grid[n - 1][m - 1] % MOD;
+    vector<vector<int>> res(n, vector<int>(m, 1));
+    int suf = 1;
     for (int i = n - 1; i >= 0; --i) {
-      for (int j = i == n - 1 ? m - 2 : m - 1; j >= 0; --j) {
-        int ii = i * m + j;
-        suf[ii] = (suf[ii + 1] * grid[i][j]) % MOD;
+      for (int j = m - 1; j >= 0; --j) {
+        res[i][j] = suf;
+        suf = (suf * (grid[i][j] % MOD)) % MOD;
       }
     }
+    int pre = 1;
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < m; ++j) {
-        int ii = i * m + j;
-        if (ii == 0) {
-          grid[i][j] = suf[ii + 1];
-        } else if (ii == n * m - 1) {
-          grid[i][j] = pre[ii - 1];
-        } else {
-          grid[i][j] = (pre[ii - 1] * suf[ii + 1]) % MOD;
-        }
+        res[i][j] = (res[i][j] * pre) % MOD;
+        pre = (pre * (grid[i][j] % MOD)) % MOD;
       }
     }
-    return grid;
+    return res;
   }
 };

647120dc9fb2da3a5d22a526795cd0f00d31d9df
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-24 20:34:04 +0000

    Improve solution to Minimum Average Difference

--- src/2256-minimum-average-difference.cpp
+++ src/2256-minimum-average-difference.cpp
@@ -10,16 +10,16 @@ class Solution {
  public:
   int minimumAverageDifference(vector<int>& nums) {
     int n = nums.size();
-    vector<long long> pre(n), suf(n);
-    pre[0] = nums[0];
-    for (int i = 1; i < n; ++i) { pre[i] = pre[i - 1] + nums[i]; }
-    suf[n - 1] = nums[n - 1];
-    for (int i = n - 2; i >= 0; --i) { suf[i] = suf[i + 1] + nums[i]; }
-    long long best = numeric_limits<int>::max();
-    int ans = -1;
+    long long total = 0;
+    for (int x : nums) { total += x; }
+    long long best = numeric_limits<long long>::max();
+    long long prefix = 0;
+    int ans = 0;
     for (int i = 0; i < n; ++i) {
-      long long diff = pre[i] / (i + 1);
-      if (i + 1 < n) { diff -= suf[i + 1] / (n - i - 1); }
+      prefix += nums[i];
+      long long suffix = total - prefix;
+      long long diff = prefix / (i + 1);
+      if (i + 1 < n) { diff -= suffix / (n - i - 1); }
       diff = abs(diff);
       if (diff < best) {
         best = diff;

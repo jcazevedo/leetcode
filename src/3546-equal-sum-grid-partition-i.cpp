8c05622cd2f973e3a8a8adb8bb05dc6bd649c8f9
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-25 00:49:25 +0000

    Improve solution to Equal Sum Grid Partition I

--- src/3546-equal-sum-grid-partition-i.cpp
+++ src/3546-equal-sum-grid-partition-i.cpp
@@ -9,25 +9,19 @@ class Solution {
  public:
   bool canPartitionGrid(vector<vector<int>>& grid) {
     int m = grid.size(), n = grid[0].size();
-    long long sum = 0L;
+    long long sum = 0L, acc;
     for (int i = 0; i < m; ++i) {
       for (int j = 0; j < n; ++j) { sum += grid[i][j]; }
     }
-    long long left = 0L, right = sum;
+    acc = 0L;
     for (int i = 0; i < m - 1; ++i) {
-      for (int j = 0; j < n; ++j) {
-        left += grid[i][j];
-        right -= grid[i][j];
-      }
-      if (left == right) { return true; }
+      for (int j = 0; j < n; ++j) { acc += grid[i][j]; }
+      if (acc * 2L == sum) { return true; }
     }
-    left = 0L, right = sum;
+    acc = 0L;
     for (int j = 0; j < n - 1; ++j) {
-      for (int i = 0; i < m; ++i) {
-        left += grid[i][j];
-        right -= grid[i][j];
-      }
-      if (left == right) { return true; }
+      for (int i = 0; i < m; ++i) { acc += grid[i][j]; }
+      if (acc * 2L == sum) { return true; }
     }
     return false;
   }

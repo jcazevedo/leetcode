fe579e8175b7f7d09c48732def40b6f529969dd2
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-28 10:11:59 +0000

    Remove usages of auto in solution to Ransom Note

--- src/0383-ransom-note.cpp
+++ src/0383-ransom-note.cpp
@@ -12,7 +12,7 @@ class Solution {
     map<char, int> ransom_cnt, magazine_cnt;
     for (char ch : ransomNote) ransom_cnt[ch]++;
     for (char ch : magazine) magazine_cnt[ch]++;
-    for (auto itr = ransom_cnt.begin(); itr != ransom_cnt.end(); ++itr) {
+    for (map<char, int>::iterator itr = ransom_cnt.begin(); itr != ransom_cnt.end(); ++itr) {
       if (magazine_cnt[itr->first] < itr->second) return false;
     }
     return true;

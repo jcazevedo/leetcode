b5de6623d2e47688a23ecc7dc13e651a4f1440ea
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-28 09:56:28 +0000

    Reformat solution to Top K Frequent Elements

--- src/0347-top-k-frequent-elements.cpp
+++ src/0347-top-k-frequent-elements.cpp
@@ -11,11 +11,9 @@ class Solution {
  public:
   vector<int> topKFrequent(vector<int>& nums, int k) {
     unordered_map<int, int> f;
-    for (auto e : nums) f[e]++;
-
+    for (auto e : nums) { f[e]++; }
     priority_queue<pair<int, int>> pq;
     for (auto e : f) { pq.push(make_pair(e.second, e.first)); }
-
     vector<int> res;
     while ((int)res.size() != k) {
       auto curr = pq.top();

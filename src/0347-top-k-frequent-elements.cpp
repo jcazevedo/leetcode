180034926aac182503fd3d4b185234acea0045b8
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-28 09:57:20 +0000

    Remove usages of auto in solution to Top K Frequent Elements

--- src/0347-top-k-frequent-elements.cpp
+++ src/0347-top-k-frequent-elements.cpp
@@ -11,12 +11,12 @@ class Solution {
  public:
   vector<int> topKFrequent(vector<int>& nums, int k) {
     unordered_map<int, int> f;
-    for (auto e : nums) { f[e]++; }
+    for (int e : nums) { f[e]++; }
     priority_queue<pair<int, int>> pq;
-    for (auto e : f) { pq.push(make_pair(e.second, e.first)); }
+    for (pair<int, int> e : f) { pq.push(make_pair(e.second, e.first)); }
     vector<int> res;
     while ((int)res.size() != k) {
-      auto curr = pq.top();
+      pair<int, int> curr = pq.top();
       pq.pop();
       res.push_back(curr.second);
     }

44104ed3c8591b7f2fcb87813ea69cd2e10df695
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-28 10:15:42 +0000

    Remove usages of auto in solution to Number of Pairs of Interchangeable Rectangles

--- src/2001-number-of-pairs-of-interchangeable-rectangles.cpp
+++ src/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -12,7 +12,8 @@ class Solution {
     unordered_map<double, long long> count;
     for (const vector<int>& rectangle : rectangles) ++count[((double)rectangle[0]) / rectangle[1]];
     long long ans = 0L;
-    for (auto itr = count.begin(); itr != count.end(); ++itr) ans += (itr->second * (itr->second - 1)) / 2L;
+    for (unordered_map<double, long long>::iterator itr = count.begin(); itr != count.end(); ++itr)
+      ans += (itr->second * (itr->second - 1)) / 2L;
     return ans;
   }
 };

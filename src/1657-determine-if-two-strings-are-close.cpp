1efe86c71ea1876c8aee70edffd7eaa7e45a2777
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-28 10:15:00 +0000

    Remove usages of auto in solution to Determine if Two Strings Are Close

--- src/1657-determine-if-two-strings-are-close.cpp
+++ src/1657-determine-if-two-strings-are-close.cpp
@@ -14,13 +14,13 @@ class Solution {
     map<char, int> cnt1, cnt2;
     for (char ch : word1) cnt1[ch]++;
     for (char ch : word2) cnt2[ch]++;
-    for (auto itr = cnt1.begin(); itr != cnt1.end(); ++itr)
+    for (map<char, int>::iterator itr = cnt1.begin(); itr != cnt1.end(); ++itr)
       if (cnt2.find(itr->first) == cnt2.end()) return false;
-    for (auto itr = cnt2.begin(); itr != cnt2.end(); ++itr)
+    for (map<char, int>::iterator itr = cnt2.begin(); itr != cnt2.end(); ++itr)
       if (cnt1.find(itr->first) == cnt1.end()) return false;
     vector<int> tot1, tot2;
-    for (auto itr = cnt1.begin(); itr != cnt1.end(); ++itr) tot1.push_back(itr->second);
-    for (auto itr = cnt2.begin(); itr != cnt2.end(); ++itr) tot2.push_back(itr->second);
+    for (map<char, int>::iterator itr = cnt1.begin(); itr != cnt1.end(); ++itr) tot1.push_back(itr->second);
+    for (map<char, int>::iterator itr = cnt2.begin(); itr != cnt2.end(); ++itr) tot2.push_back(itr->second);
     sort(tot1.begin(), tot1.end());
     sort(tot2.begin(), tot2.end());
     return tot1 == tot2;

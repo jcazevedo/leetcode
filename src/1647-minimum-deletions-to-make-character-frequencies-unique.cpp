ab6ae319e0283ce4bb0ee058ef367f7d2af7aa30
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-03-28 10:14:50 +0000

    Remove usages of auto in solution to Minimum Deletions to Make Character Frequencies Unique

--- src/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ src/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -16,9 +16,10 @@ class Solution {
     unordered_map<char, int> cnts;
     for (int i = 0; i < N; ++i) cnts[s[i]]++;
     map<int, set<char>> reverseCnts;
-    for (auto it = cnts.begin(); it != cnts.end(); ++it) reverseCnts[it->second].insert(it->first);
+    for (unordered_map<char, int>::iterator it = cnts.begin(); it != cnts.end(); ++it)
+      reverseCnts[it->second].insert(it->first);
     int ans = 0;
-    for (auto it = reverseCnts.rbegin(); it != reverseCnts.rend(); ++it) {
+    for (map<int, set<char>>::reverse_iterator it = reverseCnts.rbegin(); it != reverseCnts.rend(); ++it) {
       while (reverseCnts[it->first].size() > 1) {
         char ch = *reverseCnts[it->first].begin();
         for (int targetCnt = it->first - 1; targetCnt >= 0; --targetCnt) {

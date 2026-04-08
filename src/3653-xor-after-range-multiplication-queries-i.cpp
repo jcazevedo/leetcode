19a53bea159264dad5c26f15fda99d8381712eee
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-04-08 08:44:34 +0100

    Improve solution to XOR After Range Multiplication Queries I

--- src/3653-xor-after-range-multiplication-queries-i.cpp
+++ src/3653-xor-after-range-multiplication-queries-i.cpp
@@ -10,14 +10,12 @@ class Solution {
   int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
     long long MOD = 1e9 + 7;
     int n = nums.size();
-    vector<long long> a(n);
-    for (int i = 0; i < n; i++) { a[i] = nums[i]; }
     for (const vector<int>& q : queries) {
       int l = q[0], r = q[1], k = q[2], v = q[3];
-      for (int idx = l; idx <= r; idx += k) { a[idx] = (a[idx] * v) % MOD; }
+      for (int idx = l; idx <= r; idx += k) { nums[idx] = (1LL * nums[idx] * v) % MOD; }
     }
     int ans = 0;
-    for (int i = 0; i < n; i++) { ans ^= a[i]; }
+    for (int i = 0; i < n; i++) { ans ^= nums[i]; }
     return ans;
   }
 };

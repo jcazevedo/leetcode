fb305289f016383424578faa046c29e9a9be41db
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-04-14 09:12:44 +0100

    Improve solution to Minimum Total Distance Traveled

--- src/2463-minimum-total-distance-traveled.cpp
+++ src/2463-minimum-total-distance-traveled.cpp
@@ -7,31 +7,23 @@
 
 using namespace std;
 
-#define UNSET (-1L)
-#define INF (1e18L)
+#define INF 1000000000000000000LL
 
 class Solution {
- private:
-  long long minimumDistance(
-      int r, int f, const vector<int>& robot, const vector<int>& factory, vector<vector<long long>>& cache) {
-    if (r >= (int)robot.size()) { return 0L; }
-    if (f >= (int)factory.size()) { return INF; }
-    if (cache[r][f] != UNSET) { return cache[r][f]; }
-    long long best = min(abs(robot[r] - factory[f]) + minimumDistance(r + 1, f + 1, robot, factory, cache),
-                         minimumDistance(r, f + 1, robot, factory, cache));
-    cache[r][f] = best;
-    return best;
-  }
-
  public:
   long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
     sort(begin(robot), end(robot));
     sort(begin(factory), end(factory));
-    vector<int> factoryPositions;
+    vector<int> fp;
     for (const vector<int>& f : factory) {
-      for (int i = 0; i < f[1]; ++i) { factoryPositions.push_back(f[0]); }
+      for (int i = 0; i < f[1]; ++i) { fp.push_back(f[0]); }
     }
-    vector<vector<long long>> cache(robot.size(), vector<long long>(factoryPositions.size(), UNSET));
-    return minimumDistance(0, 0, robot, factoryPositions, cache);
+    int R = robot.size(), F = fp.size();
+    vector<vector<long long>> dp(R + 1, vector<long long>(F + 1, INF));
+    for (int f = 0; f <= F; ++f) { dp[R][f] = 0; }
+    for (int r = R - 1; r >= 0; --r) {
+      for (int f = F - 1; f >= 0; --f) { dp[r][f] = min(abs(robot[r] - fp[f]) + dp[r + 1][f + 1], dp[r][f + 1]); }
+    }
+    return dp[0][0];
   }
 };

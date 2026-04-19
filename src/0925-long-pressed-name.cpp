f876aaf29873d6a0a8b63f5a35ce5d07349b87b3
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-04-19 09:41:57 +0100

    Improve solution to Long Pressed Name

--- src/0925-long-pressed-name.cpp
+++ src/0925-long-pressed-name.cpp
@@ -8,22 +8,14 @@ using namespace std;
 class Solution {
  public:
   bool isLongPressedName(string name, string typed) {
-    int N = name.size(), T = typed.size(), ni = 0, ti = 0;
-    while (ni < N && ti < T) {
-      if (name[ni] != typed[ti]) { return false; }
-      int nc = 0, tc = 0;
-      while (ni + 1 < N && name[ni + 1] == name[ni]) {
-        ++ni;
-        ++nc;
+    int i = 0, N = name.size(), T = typed.size();
+    for (int j = 0; j < T; ++j) {
+      if (i < N && name[i] == typed[j]) {
+        ++i;
+      } else if (j == 0 || typed[j] != typed[j - 1]) {
+        return false;
       }
-      while (ti + 1 < T && typed[ti + 1] == typed[ti]) {
-        ++ti;
-        ++tc;
-      }
-      if (nc > tc) { return false; }
-      ++ni;
-      ++ti;
     }
-    return ni >= N && ti >= T;
+    return i == N;
   }
 };

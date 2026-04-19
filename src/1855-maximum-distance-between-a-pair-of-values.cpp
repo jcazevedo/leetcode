84de3883e314294ae5784be663d3bc283319f07a
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-04-19 08:54:10 +0100

    Add solution to Maximum Distance Between a Pair of Values

---
+++ src/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,19 @@
+// 1855. Maximum Distance Between a Pair of Values
+// https://leetcode.com/problems/maximum-distance-between-a-pair-of-values/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+    int n1 = nums1.size(), n2 = nums2.size(), i = 0, ans = 0;
+    for (int j = 0; j < n2; ++j) {
+      while (i < n1 && nums2[j] < nums1[i]) { ++i; }
+      if (i < n1 && nums2[j] >= nums1[i]) { ans = max(ans, j - i); }
+    }
+    return ans;
+  }
+};

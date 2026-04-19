7701586683fea55eb5ebce2261b8e1b49ba7271d
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-04-19 10:07:26 +0100

    Improve solution to Reverse Vowels of a String

--- src/0345-reverse-vowels-of-a-string.cpp
+++ src/0345-reverse-vowels-of-a-string.cpp
@@ -2,17 +2,14 @@
 // https://leetcode.com/problems/reverse-vowels-of-a-string/
 
 #include <algorithm>
-#include <cctype>
 #include <string>
+#include <string_view>
 
 using namespace std;
 
 class Solution {
  private:
-  bool isVowel(char ch) {
-    char c = tolower(ch);
-    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-  }
+  static bool isVowel(char c) { return string_view("aeiouAEIOU").find(c) != string_view::npos; }
 
  public:
   string reverseVowels(string s) {
@@ -20,7 +17,6 @@ class Solution {
     while (l < r) {
       while (l < r && !isVowel(s[l])) { ++l; }
       while (l < r && !isVowel(s[r])) { --r; }
-      if (l >= r) { break; }
       swap(s[l], s[r]);
       ++l;
       --r;

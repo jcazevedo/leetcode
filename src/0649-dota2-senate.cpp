c5fbb746e7da44d21a985230ad79f069673e280f
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-04-23 16:28:44 +0100

    Add solution to Dota2 Senate

---
+++ src/0649-dota2-senate.cpp
@@ -0,0 +1,34 @@
+// 649. Dota2 Senate
+// https://leetcode.com/problems/dota2-senate/
+
+#include <queue>
+#include <string>
+
+using namespace std;
+
+class Solution {
+ public:
+  string predictPartyVictory(string senate) {
+    int n = senate.size();
+    queue<int> radiant, dire;
+    for (int i = 0; i < n; ++i) {
+      if (senate[i] == 'R') {
+        radiant.push(i);
+      } else {
+        dire.push(i);
+      }
+    }
+    while (!radiant.empty() && !dire.empty()) {
+      int r = radiant.front();
+      int d = dire.front();
+      radiant.pop();
+      dire.pop();
+      if (r < d) {
+        radiant.push(r + n);
+      } else {
+        dire.push(d + n);
+      }
+    }
+    return radiant.empty() ? "Dire" : "Radiant";
+  }
+};

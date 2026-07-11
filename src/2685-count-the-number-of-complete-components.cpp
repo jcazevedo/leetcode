b609bf64bfd008d9338a815c7550197dca0735c3
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-07-11 13:25:13 +0100

    Improve solution to Count the Number of Complete Components

--- src/2685-count-the-number-of-complete-components.cpp
+++ src/2685-count-the-number-of-complete-components.cpp
@@ -1,43 +1,40 @@
 // 2685. Count the Number of Complete Components
 // https://leetcode.com/problems/count-the-number-of-complete-components/
 
-#include <algorithm>
+#include <stack>
 #include <vector>
 
 using namespace std;
 
 class Solution {
- private:
-  void dfs(int u, int c, const vector<vector<int>>& graph, vector<int>& component) {
-    component[u] = c;
-    for (int v : graph[u]) {
-      if (component[v] != -1) { continue; }
-      dfs(v, c, graph, component);
-    }
-  }
-
  public:
   int countCompleteComponents(int n, vector<vector<int>>& edges) {
-    vector<vector<int>> graph(n, vector<int>());
+    vector<vector<int>> graph(n);
     for (const vector<int>& edge : edges) {
       graph[edge[0]].push_back(edge[1]);
       graph[edge[1]].push_back(edge[0]);
     }
-    vector<int> component(n, -1);
-    int ans = 0, components = 0;
+    vector<bool> visited(n, false);
+    int ans = 0;
     for (int i = 0; i < n; ++i) {
-      if (component[i] != -1) { continue; }
-      dfs(i, components, graph, component);
-      bool good = true;
-      for (int j = 0; j < n && good; ++j) {
-        if (component[j] != components) { continue; }
-        for (int k = 0; k < n && good; ++k) {
-          if (j == k || component[k] != components) { continue; }
-          if (find(graph[j].begin(), graph[j].end(), k) == graph[j].end()) { good = false; }
+      if (visited[i]) { continue; }
+      int vertexCount = 0;
+      long long degreeSum = 0;
+      stack<int> s;
+      s.push(i);
+      visited[i] = true;
+      while (!s.empty()) {
+        const int u = s.top();
+        s.pop();
+        ++vertexCount;
+        degreeSum += graph[u].size();
+        for (int v : graph[u]) {
+          if (visited[v]) { continue; }
+          visited[v] = true;
+          s.push(v);
         }
       }
-      if (good) { ++ans; }
-      ++components;
+      if (degreeSum == 1LL * vertexCount * (vertexCount - 1)) { ++ans; }
     }
     return ans;
   }

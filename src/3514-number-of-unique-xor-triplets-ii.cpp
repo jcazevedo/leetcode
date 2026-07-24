9e08cbbc8a6caae93e96eaac27b106b1539a700f
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-07-24 10:03:28 +0100

    Improve solution to Number of Unique XOR Triplets II

--- src/3514-number-of-unique-xor-triplets-ii.cpp
+++ src/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,7 +1,7 @@
 // 3514. Number of Unique XOR Triplets II
 // https://leetcode.com/problems/number-of-unique-xor-triplets-ii/
 
-#include <unordered_set>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -9,15 +9,26 @@ using namespace std;
 class Solution {
  public:
   int uniqueXorTriplets(vector<int>& nums) {
-    int N = nums.size();
-    unordered_set<int> pairs;
-    for (int i = 0; i < N; ++i) {
-      for (int j = i; j < N; ++j) { pairs.insert(nums[i] ^ nums[j]); }
+    int largest = 0;
+    for (int v : nums) { largest = max(v, largest); }
+    int k = 1;
+    while (k <= largest) { k <<= 1; }
+    vector<bool> one(k), two(k), three(k);
+    for (int v : nums) {
+      one[v] = true;
+      for (int i = 0; i < k; ++i) {
+        if (one[i]) { two[v ^ i] = true; }
+      }
     }
-    unordered_set<int> triplets;
-    for (int i = 0; i < N; ++i) {
-      for (int p : pairs) { triplets.insert(nums[i] ^ p); }
+    for (int v : nums) {
+      for (int i = 0; i < k; ++i) {
+        if (two[i]) { three[v ^ i] = true; }
+      }
     }
-    return triplets.size();
+    int ans = 0;
+    for (int i = 0; i < k; ++i) {
+      if (three[i]) { ++ans; }
+    }
+    return ans;
   }
 };

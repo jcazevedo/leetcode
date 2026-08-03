12f249f52b5c529b4ac6be8c42aab3b33e480bce
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-08-03 11:51:27 +0100

    Reformat solution to Stone Game III

--- src/1406-stone-game-iii.cpp
+++ src/1406-stone-game-iii.cpp
@@ -12,16 +12,17 @@ using namespace std;
 class Solution {
  private:
   int getAliceScoreAux(int player, int startAt, const vector<int>& stoneValue, vector<vector<int>>& cache) {
-    if (startAt >= (int)stoneValue.size()) return 0;
+    if (startAt >= (int)stoneValue.size()) { return 0; }
     if (cache[player][startAt] == -1) {
       int ans = player == 0 ? numeric_limits<int>::min() : numeric_limits<int>::max();
       int sumSoFar = 0;
       for (int i = startAt; i < min(startAt + 3, (int)stoneValue.size()); ++i) {
         sumSoFar += stoneValue[i];
-        if (player == 0)
+        if (player == 0) {
           ans = max(ans, sumSoFar + getAliceScoreAux(player ^ 1, i + 1, stoneValue, cache));
-        else
+        } else {
           ans = min(ans, getAliceScoreAux(player ^ 1, i + 1, stoneValue, cache));
+        }
       }
       cache[player][startAt] = ans;
     }
@@ -37,8 +38,8 @@ class Solution {
   string stoneGameIII(vector<int>& stoneValue) {
     int totalScore = accumulate(stoneValue.begin(), stoneValue.end(), 0);
     int aliceScore = getAliceScore(stoneValue);
-    if (totalScore - aliceScore == aliceScore) return "Tie";
-    if (totalScore - aliceScore > aliceScore) return "Bob";
+    if (totalScore - aliceScore == aliceScore) { return "Tie"; }
+    if (totalScore - aliceScore > aliceScore) { return "Bob"; }
     return "Alice";
   }
 };

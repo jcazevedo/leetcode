7c6c68d343409a27c42e5e7e75495801e459bae7
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-08-24 14:17:31 +0100

    Add solution to Stone Game VIII

---
+++ src/1872-stone-game-viii.cpp
@@ -0,0 +1,20 @@
+// 1872. Stone Game VIII
+// https://leetcode.com/problems/stone-game-viii/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int stoneGameVIII(vector<int>& stones) {
+    int n = stones.size();
+    vector<long long> prefix(n);
+    prefix[0] = stones[0];
+    for (int i = 1; i < n; ++i) { prefix[i] = prefix[i - 1] + stones[i]; }
+    long long best = prefix[n - 1];
+    for (int i = n - 2; i >= 1; --i) { best = max(best, prefix[i] - best); }
+    return best;
+  }
+};

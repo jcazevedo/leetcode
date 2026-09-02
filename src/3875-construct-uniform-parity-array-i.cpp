6d9b8f23aef3e2a65c915fbe7337e4049f07b716
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-09-02 16:00:22 +0100

    Add solution to Construct Uniform Parity Array I

---
+++ src/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,11 @@
+// 3875. Construct Uniform Parity Array I
+// https://leetcode.com/problems/construct-uniform-parity-array-i/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  bool uniformArray(vector<int>& nums1) { return true; }
+};

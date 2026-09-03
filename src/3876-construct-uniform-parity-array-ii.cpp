3466db7f22bd74f5b6476b4ac33724fa98a71b66
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-09-03 11:30:34 +0100

    Improve solution to Construct Uniform Parity Array II

--- src/3876-construct-uniform-parity-array-ii.cpp
+++ src/3876-construct-uniform-parity-array-ii.cpp
@@ -1,41 +1,31 @@
 // 3876. Construct Uniform Parity Array II
 // https://leetcode.com/problems/construct-uniform-parity-array-ii/
 
-#include <set>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
 class Solution {
- private:
-  bool allEven(const vector<int>& nums1, const set<int>& odds) {
-    for (int num : nums1) {
-      if (num % 2 == 1) {
-        set<int>::iterator itr = odds.lower_bound(num);
-        if (itr == odds.begin()) { return false; }
-      }
-    }
-    return true;
-  }
-
-  bool allOdd(const vector<int>& nums1, const set<int>& odds) {
+ public:
+  bool uniformArray(vector<int>& nums1) {
+    int minOdd = -1, minEven = -1, oddCount = 0;
     for (int num : nums1) {
       if (num % 2 == 0) {
-        set<int>::iterator itr = odds.lower_bound(num);
-        if (itr == odds.begin()) {
-          if (*odds.begin() >= num) { return false; }
+        if (minEven == -1) {
+          minEven = num;
+        } else {
+          minEven = min(minEven, num);
+        }
+      } else {
+        ++oddCount;
+        if (minOdd == -1) {
+          minOdd = num;
+        } else {
+          minOdd = min(minOdd, num);
         }
       }
     }
-    return true;
-  }
-
- public:
-  bool uniformArray(vector<int>& nums1) {
-    set<int> odds;
-    for (int num : nums1) {
-      if (num % 2 == 1) { odds.insert(num); }
-    }
-    return allEven(nums1, odds) || allOdd(nums1, odds);
+    return oddCount >= 2 || minOdd < minEven;
   }
 };

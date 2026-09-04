186b5a3ef6d8cf54cc65d69c3e02685c5a8fc42e
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-09-04 13:14:51 +0100

    Improve solution to Smallest Stable Index I

--- src/3903-smallest-stable-index-i.cpp
+++ src/3903-smallest-stable-index-i.cpp
@@ -10,13 +10,13 @@ class Solution {
  public:
   int firstStableIndex(vector<int>& nums, int k) {
     int n = nums.size();
-    vector<int> maxV(n, -1), minV(n, -1);
-    maxV[0] = nums[0];
-    for (int i = 1; i < n; ++i) { maxV[i] = max(maxV[i - 1], nums[i]); }
+    vector<int> minV(n);
     minV[n - 1] = nums[n - 1];
     for (int i = n - 2; i >= 0; --i) { minV[i] = min(minV[i + 1], nums[i]); }
+    int maxV = -1;
     for (int i = 0; i < n; ++i) {
-      int score = maxV[i] - minV[i];
+      maxV = max(maxV, nums[i]);
+      int score = maxV - minV[i];
       if (score > k) { continue; }
       return i;
     }

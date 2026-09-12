e5b0817f4a24bc7896aa5edb2705c88b12a68eb0
Joao Azevedo <joao.c.azevedo@gmail.com>
2026-09-12 12:25:32 +0100

    Add solution to Smallest Stable Index II

---
+++ src/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,25 @@
+// 3904. Smallest Stable Index II
+// https://leetcode.com/problems/smallest-stable-index-ii/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int firstStableIndex(vector<int>& nums, int k) {
+    int n = nums.size();
+    vector<int> minV(n);
+    minV[n - 1] = nums[n - 1];
+    for (int i = n - 2; i >= 0; --i) { minV[i] = min(minV[i + 1], nums[i]); }
+    int maxV = -1;
+    for (int i = 0; i < n; ++i) {
+      maxV = max(maxV, nums[i]);
+      int score = maxV - minV[i];
+      if (score > k) { continue; }
+      return i;
+    }
+    return -1;
+  }
+};
